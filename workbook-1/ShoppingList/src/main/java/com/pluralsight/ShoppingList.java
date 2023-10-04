cb4e963cf6a2585f588f6c38336fc985afd71595
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-10-04 09:43:20 -0400

    completed ShoppingList app

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apple");
+        System.out.println("Oat Milk");
+        System.out.println("Carrot");
+        System.out.println("Spinach");
+        System.out.println("Onion");
+        System.out.println("Grape");
+        System.out.println("Rice");
+        System.out.println("Cookies");
+        System.out.println("Chips");
+        System.out.println("Cheese");
+    }
+}
