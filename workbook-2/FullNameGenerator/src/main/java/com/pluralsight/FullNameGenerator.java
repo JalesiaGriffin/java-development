a271e0d87dc79d6b50d1ab4496698d15e039d900
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-10-10 11:03:15 -0400

    Completed Module 1 - Excercise 1 - FullNameGenerator

--- workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameGenerator.java
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralsight/FullNameGenerator.java
@@ -1,7 +1,33 @@
 package com.pluralsight;
 
+import java.util.Scanner;
 public class FullNameGenerator {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        // Prompt
+        System.out.println("Please enter your name");
+        System.out.print("First name: ");
+        String firstName = scan.nextLine();
+
+        System.out.print("Middle name (or press enter): ");
+        String middleName = scan.nextLine();
+
+        System.out.print("Last name: ");
+        String lastName = scan.nextLine();
+
+        System.out.println("Suffix (or press enter): ");
+        String suffix = scan.nextLine();
+
+        // Conditions
+        if (middleName.equals("") && suffix.equals("")) {
+            System.out.println("Full name: " + firstName + " " + lastName);
+        } else if (middleName.equals("")) {
+            System.out.println("Full name: " + firstName + " " + lastName + ", "+ suffix);
+        } else if (!middleName.equals("") && suffix.equals("")) {
+            System.out.println("Full name: " + firstName + " " + middleName.charAt(0) + ". " + lastName);
+        } else {
+            System.out.println("Full name: " + firstName + " " + middleName.charAt(0) + ". " + lastName + ", " + suffix);
+        }
     }
 }
