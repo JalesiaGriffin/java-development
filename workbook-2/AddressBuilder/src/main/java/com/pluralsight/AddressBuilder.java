81393983cc2b13d0b0e0dfee6a000e41d2990234
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-10-11 10:35:37 -0400

    Completed Module 1 - Exercise 5 - AddressBuilder

--- workbook-2/AddressBuilder/src/main/java/com/pluralsight/AddressBuilder.java
+++ workbook-2/AddressBuilder/src/main/java/com/pluralsight/AddressBuilder.java
@@ -1,4 +1,45 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AddressBuilder {
+    public static void main(String[] args) {
+        // Create Objects
+        StringBuilder address = new StringBuilder();
+        Scanner scan = new Scanner(System.in);
+
+        //Prompts and appends
+        System.out.println("Please provide the following information:");
+        System.out.print("Full name: ");
+        address.append(scan.nextLine() + "\n\n");
+
+        address.append("Billing Address:\n");
+
+        System.out.print("Billing Street: ");
+        address.append(scan.nextLine() + "\n");
+
+        System.out.print("Billing City: ");
+        address.append(scan.nextLine() + ", ");
+
+        System.out.print("Billing State: ");
+        address.append(scan.nextLine() + " ");
+
+        System.out.print("Billing Zip: ");
+        address.append(scan.nextLine());
+
+        address.append("\n\nShipping Address:\n");
+        System.out.print("\nShipping Street: ");
+        address.append(scan.nextLine() + "\n");
+
+        System.out.print("Shipping City: ");
+        address.append(scan.nextLine() + ", ");
+
+        System.out.print("Shipping State: ");
+        address.append(scan.nextLine() + " ");
+
+        System.out.print("Shipping Zip: ");
+        address.append(scan.nextLine());
+
+        System.out.println(address);
+    }
 }
