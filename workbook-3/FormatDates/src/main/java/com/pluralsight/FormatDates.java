22fb21a993db463f434b197fbc36311f265492d2
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-10-17 13:30:42 -0400

    Completed Mod 3 - Exercise 1 - Format Dates

--- workbook-3/FormatDates/src/main/java/com/pluralsight/FormatDates.java
+++ workbook-3/FormatDates/src/main/java/com/pluralsight/FormatDates.java
@@ -1,2 +1,28 @@
-package com.pluralsight;public class FormatDates {
+package com.pluralsight;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class FormatDates {
+    public static void main(String[] args) {
+        // Get date, time, and timezone
+        LocalDate date = LocalDate.now();
+        ZonedDateTime gmt = ZonedDateTime.now(ZoneId.of("GMT"));
+
+        // Create formatters
+        DateTimeFormatter dateFormatter1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy hh:mm");
+
+        // Format
+        String date2 = date.format(dateFormatter1);
+        String date3 = date.format(dateTimeFormatter2);
+        String dateAndTime = gmt.format(fmt);
+
+        // Print
+        System.out.println(date2);
+        System.out.println(date);
+        System.out.println(date3);
+        System.out.println(dateAndTime);
+    }
 }
