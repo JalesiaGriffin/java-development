8547ff857873713c3bdc7d319a928017d482ebc6
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-12-07 13:25:51 -0500

    updated

--- workbook-7/joins/mod4-exercise3.sql
+++ workbook-7/joins/mod4-exercise3.sql
@@ -1,31 +1,29 @@
 USE northwind;
 
 -- Question 1
-SELECT products.ProductID, products.ProductName, 
-	   products.UnitPrice, CategoryName
-FROM northwind.products
-JOIN categories
-	ON products.CategoryID = categories.CategoryID
-ORDER BY CategoryName, ProductName;
+SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CategoryName
+FROM products p
+JOIN categories c
+	ON p.CategoryID = c.CategoryID
+ORDER BY c.CategoryName, p.ProductName;
 
 -- Question 2
-SELECT products.ProductID, products.ProductName, 
-	   products.UnitPrice, CompanyName
-FROM northwind.products
-JOIN suppliers
-	ON products.SupplierID = suppliers.SupplierID
+SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CompanyName
+FROM products p
+JOIN suppliers s
+	ON p.SupplierID = s.SupplierID
 WHERE UnitPrice >= 75
-ORDER BY ProductName;
+ORDER BY p.ProductName;
 
 -- Question 3
-SELECT products.ProductID, products.ProductName, 
-	   products.UnitPrice, CategoryName, CompanyName
-FROM northwind.products
-JOIN categories
-	ON products.CategoryID = categories.CategoryID
-JOIN suppliers
-	ON products.SupplierID = suppliers.SupplierID
-ORDER BY ProductName;
+SELECT p.ProductID, p.ProductName, 
+	   p.UnitPrice, c.CategoryName, s.CompanyName
+FROM products p
+JOIN categories c
+	ON p.CategoryID = c.CategoryID
+JOIN suppliers s
+	ON p.SupplierID = p.SupplierID
+ORDER BY p.ProductName;
 
 -- Question 4
 SELECT p.ProductName, c.CategoryName
@@ -41,18 +39,16 @@ JOIN shippers s ON s.ShipperID = o.ShipVia
 WHERE o.ShipCountry = "Germany";
 
 -- Question 6
-SELECT orders.OrderID, orders.OrderDate, orders.ShipName, orders.ShipAddress
-FROM northwind.orders
+SELECT o.OrderID, o.OrderDate, o.ShipName, o.ShipAddress
+FROM orders o
 WHERE OrderID IN (SELECT OrderID
-				  FROM northwind.`order details`
+				  FROM `order details` od
                   WHERE ProductID = (SELECT ProductID
 									FROM northwind.products
                                     WHERE ProductName = "Sasquatch Ale"));
-                                    
-SELECT `Order Details`.OrderID, OrderDate, ShipName, ShipAddress 
-FROM `Order Details`
-JOIN Orders
-	ON `Order Details`.OrderID = Orders.OrderID
-JOIN Products
-	ON `Order Details`.ProductID = Products.ProductID
-WHERE ProductName LIKE "Sasquatch Ale";
+
+SELECT o.OrderID, o.OrderDate, o.ShipName, o.ShipAddress
+FROM orders od
+JOIN `order details` od ON o.OrderID = od.OrderID
+JOIN products p ON od.ProductID = p.ProductID
+WHERE p.ProductName = "Sasquatch Ale";
